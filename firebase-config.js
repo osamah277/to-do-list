3f8a00774da8724dabe3db4b5f44799c017358e5
Osamah <77911801+osamah277@users.noreply.github.com>
2025-06-01 21:08:24 +0000

    update

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyA3FUbZhG2qA-lsVVpUJR4UEebhJQBIGLI",
+  authDomain: "todolist-7c38d.firebaseapp.com",
+  projectId: "todolist-7c38d",
+  storageBucket: "todolist-7c38d.appspot.com",
+  messagingSenderId: "1017408235677",
+  appId: "1:1017408235677:web:b5479bc8545ee4cdfa8c74"
+};
+
+firebase.initializeApp(firebaseConfig);
